d7d3a64c82e0e0e17d0d9c2de29ee87fec0f52fc
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-20 09:21:05 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by artsw0rd on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
